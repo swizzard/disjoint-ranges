06c046597d5ba2eb32b865c69bbdeb040eede245
Sam Raker <sam.raker@gmail.com>
2026-01-18 23:16:21 -0500

    update readme, update lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
+//! Unary and Disjoint ranges plus some useful traits
+
+/// Trait implementations for standard numeric types
 pub mod impls;
+/// Contiguous and disjoint ranges
 pub mod ranges;
+/// Helpful traits
 pub mod traits;
 
 pub use ranges::{DisjointRange, UnaryRange};
